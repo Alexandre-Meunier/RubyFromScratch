e922e066690e0faa24c9f83da400fc713e157f02
Alexandre Meunier <alexaudmeunier@outlook.fr>
2025-02-19 10:47:28 +0100

    Validations of products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+    assert product.errors[:description].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "title", description: "yyy", image_url: "zzz.jpg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+
+  def new_product(image_url)
+    Product.new(title: "title", description: "yyy", image_url: image_url, price: 1)
+  end
+  
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png .png}
+    bad = %w{ fred.docs fred.beskarfox}
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} must be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} must be invalid"
+    end
+    assert true
+  end
+
+  test "product is not vqlid without a unique title" do
+    product = Product.new(title: products(:one).title, description: "rf", price: 1, image_url: "fe")
+    assert product.invalid?
+  end
 end
